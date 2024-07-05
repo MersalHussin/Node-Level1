e28595c7b271b94e194f697b9db840c9f0d5e105
mersalhussin <mersalhussin@gmail.com>
2024-07-05 14:20:11 +0300

    Lesson 2

--- app.js
+++ app.js
@@ -2,11 +2,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html" , {root:__dirname})
 })
 
-app.listen(port, () => {
+
+mongoose.connect('mongodb+srv://mersalhussin:mbaCbBcy17CMYdbz@mersalhussin.oeujrem.mongodb.net/all-data?retryWrites=true&w=majority&appName=MersalHussin')
+.then( () => {app.listen(port, () => {
   console.log(`http://localhost:${port}/`)
+})
+})
+.catch( (err) => {
+  console.log("error 1")
 })
